80335183d09e7a38468f1678da6dfd1dd3ff5a17
yanfengneng <2412553598@qq.com>
2023-02-13 19:00:00 +0800

    2023/2/13

---
+++ Cpp Primer/chapter_1/io_test.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char c;
+    c=getchar();
+    cout<<c<<endl;
+    
+    system("pause");
+
+    cout<<"hello world!"<<endl;
+    
+    while((c=getchar())!='\n')
+    {
+        printf("%c",c);
+    }
+
+    cout<<endl;
+    system("pause");
+
+    return 0;
+}
