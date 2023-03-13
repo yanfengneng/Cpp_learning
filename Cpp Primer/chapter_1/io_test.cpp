f5382c49c19548bc0ff0407eed5ee77113c79393
yanfengneng <2412553598@qq.com>
2023-03-13 17:10:50 +0800

    更新 IO 中的 cin 和 cout 理解

--- Cpp Primer/chapter_1/io_test.cpp
+++ Cpp Primer/chapter_1/io_test.cpp
@@ -1,23 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void test_cin_cout()
+{
+    int x;cin>>x;
+    cout<<x<<endl;
+}
+
+void test_cout_size()
+{
+    ofstream of("test.txt");// 创建文件 test.txt 并打开
+    for(int i=0;i<4096;i++){
+        of<<'a';
+    }
+    system("pause");// 暂停
+    of<<'b';
+    system("pause");// 暂停
+}
+
 int main()
 {
-    char c;
-    c=getchar();
-    cout<<c<<endl;
-    
-    system("pause");
-
-    cout<<"hello world!"<<endl;
-    
-    while((c=getchar())!='\n')
-    {
-        printf("%c",c);
-    }
-
-    cout<<endl;
-    system("pause");
-
+    //test_cin_cout();
+    test_cout_size();
     return 0;
 }
