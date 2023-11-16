77d7ec73aa6cd14a58d9a550f70f57e832e5996d
yanfengneng <2412553598@qq.com>
2023-11-16 20:25:40 +0800

    构造函数总结

--- CppGuide/01CppBasic/left_right_value_reference.cpp
+++ CppGuide/01CppBasic/left_right_value_reference.cpp
@@ -75,9 +75,9 @@ void change(int&& right_value) {
     right_value = 8;
 }
 
-void f(const int& n) {
-    n += 1; // 编译失败，const左值引用不能修改指向变量
-}
+// void f(const int& n) {
+//     n += 1; // 编译失败，const左值引用不能修改指向变量
+// }
 
 void f2(int && n) {
     n += 1; // ok
@@ -116,23 +116,23 @@ int main()
     // int &&b = static_cast<int&&> (a);
     // cout << b << endl;
     // cout << a << endl; // 打印结果：5
-    // int a = 5; // a是个左值
-    // int &ref_a_left = a; // ref_a_left是个左值引用
-    // int &&ref_a_right = std::move(a); // ref_a_right是个右值引用
+    int a = 5; // a是个左值
+    int &ref_a_left = a; // ref_a_left是个左值引用
+    int &&ref_a_right = std::move(a); // ref_a_right是个右值引用
  
-    // // change(a); // 编译不过，a是左值，change参数要求右值
-    // // change(ref_a_left); // 编译不过，左值引用ref_a_left本身也是个左值
-    // // change(ref_a_right); // 编译不过，右值引用ref_a_right本身也是个左值
+    // change(a); // 编译不过，a是左值，change参数要求右值
+    // change(ref_a_left); // 编译不过，左值引用ref_a_left本身也是个左值
+    // change(ref_a_right); // 编译不过，右值引用ref_a_right本身也是个左值
      
-    // change(std::move(a)); // 编译通过
-    // change(std::move(ref_a_right)); // 编译通过
-    // change(std::move(ref_a_left)); // 编译通过
+    change(std::move(a)); // 编译通过
+    change(std::move(ref_a_right)); // 编译通过
+    change(std::move(ref_a_left)); // 编译通过
  
-    // change(5); // 当然可以直接接右值，编译通过
+    change(5); // 当然可以直接接右值，编译通过
      
-    // cout << &a << ' ';
-    // cout << &ref_a_left << ' ';
-    // cout << &ref_a_right;
+    cout << &a << ' ' << a << endl;;
+    cout << &ref_a_left << ' ' << ref_a_left << endl;
+    cout << &ref_a_right << ' ' << ref_a_right << endl;
     // 打印这三个左值的地址，都是一样的
     return 0;
 }
